ea6ff9f64c8892316f84c32a4f8ca0f1d1f50e73
oHaThiHoan <ha.thi.hoan@framgia.com>
2018-12-05 10:08:24 +0700

    Add login facebook project

---
+++ LogInFacebookExample/LogInFacebookExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogInFacebookExample
+//
+//  Created by Ha Thi Hoan on 12/4/18.
+//  Copyright © 2018 Ha Thi Hoan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
