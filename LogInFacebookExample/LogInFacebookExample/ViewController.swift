9bc7fc76c702d1c2643be5442f40d81bd5d5c651
oHaThiHoan <ha.thi.hoan@framgia.com>
2018-12-05 10:38:00 +0700

    Login with facebook

--- LogInFacebookExample/LogInFacebookExample/ViewController.swift
+++ LogInFacebookExample/LogInFacebookExample/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = FBSDKLoginButton.init()
+        loginButton.center = self.view.center
+        self.view.addSubview(loginButton)
     }
 
 
